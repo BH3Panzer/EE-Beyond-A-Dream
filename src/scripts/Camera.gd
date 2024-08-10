63b6e67da3c3884f863d2674e21c807ee85dc00e
BH3Panzer <clementcattin74@proton.me>
2024-08-10 14:52:46 +0200

    project importation

---
+++ src/scripts/Camera.gd
@@ -0,0 +1,11 @@
+extends Camera3D
+
+@export var camera_speed = 5
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if get_viewport().get_mouse_position() >
